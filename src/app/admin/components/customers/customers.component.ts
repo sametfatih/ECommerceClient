077c7a4ab80c44a327e4d0cec3d9df488619b431
sametfatih <samet.fatih.gozum@hotmail.com>
2023-03-02 01:21:32 +0300

    spinner added

--- src/app/admin/components/customers/customers.component.ts
+++ src/app/admin/components/customers/customers.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { NgxSpinnerService } from 'ngx-spinner';
+
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.scss']
 })
-export class CustomersComponent {
-
+export class CustomersComponent extends BaseComponent {
+  constructor(spinner:NgxSpinnerService){
+    super(spinner)
+  }
+  ngOnInit(): void {
+    this.showSpinner(SpinnerType.BallAtom);
+  }
 }

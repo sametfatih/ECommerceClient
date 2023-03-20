633423d28ca7d1257bd0c2406fbffec493cab6b1
sametfatih <samet.fatih.gozum@hotmail.com>
2023-03-20 22:29:42 +0300

    jwt

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { first, firstValueFrom, Observable } from 'rxjs';
+import { Token } from 'src/app/contracts/token/token';
+import { TokenResponse } from 'src/app/contracts/token/tokenResponse';
 import { Create_User } from 'src/app/contracts/users/create_user';
 import { User } from 'src/app/entities/user';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
 import { HttpclientService } from '../httpclient.service';
 
 @Injectable({
@@ -9,7 +12,7 @@ import { HttpclientService } from '../httpclient.service';
 })
 export class UserService {
 
-  constructor(private httpClientService: HttpclientService) { }
+  constructor(private httpClientService: HttpclientService,private toastrService: CustomToastrService) { }
 
   async create(user: User,callback?:()=> void): Promise<Create_User> {
     const observable: Observable<Create_User | User> = this.httpClientService.post<Create_User | User>({
@@ -21,8 +24,8 @@ export class UserService {
     return result 
   }
   async login(usernameOrEmail: string, password: string,
-    callback?: () => void): Promise<void> {
-    const observable: Observable<any> = this.httpClientService.post({
+    callback?: () => void): Promise<any> {
+    const observable: Observable<any | TokenResponse> = this.httpClientService.post<any | TokenResponse>({
       action: "login",
       controller: "users"
     }, {
@@ -30,7 +33,16 @@ export class UserService {
       password
     });
 
-    await firstValueFrom(observable);
+    const tokenResponse : TokenResponse = await firstValueFrom(observable)as TokenResponse;
+    if(tokenResponse){
+
+      localStorage.setItem("accessToken", tokenResponse.token.accessToken);
+
+      this.toastrService.message("Kullanıcı girişi başarıyla sağlanmıştır.","Giriş Başarılı",{
+        messageType:ToastrMessageType.Success,
+        position: ToastrPosition.TopRight
+      })
+    }
     callback();
   }
 }
